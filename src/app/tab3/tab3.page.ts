067e209d86a558beb9fdb58601c530e7d3694e67
Kimberly1414 <kimberly.arizo@evsu.edu.ph>
2021-04-23 16:39:02 +0800

    commit files

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,47 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  contName="";
+  contNumber="";
+  contacts: any = [];
+  constructor(public alertCtrl: AlertController)  {}
+
+  save() {
+    let contact = {
+      name: this.contName,
+      number: this.contNumber
+    }
+    this.contacts.push(contact);
+    console.log(this.contacts);
+    this.clearField();
+  }
+  clearField() {
+    this.contName = "";
+    this.contNumber = "";
+  }
+  async confirm(cont) {
+    const confirm = await this.alertCtrl.create({
+      header: 'Confirm!',
+      message: 'Please confirm Deletion?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+          }
+        }, {
+          text: 'Confirm',
+          handler: () => {
+            let index = this.contacts.indexOf(cont);
+            if(index > -1){
+              this.contacts.splice(index,1);
+            }
+        }
+        }
+      ]
+    } );
+
+    await confirm.present();
+  }
 
 }
