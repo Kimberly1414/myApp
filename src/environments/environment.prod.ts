fb9ea054bbd6b9f8c85c6828f1eb54b46d07eccb
Kimberly1414 <kimberly.arizo@evsu.edu.ph>
2021-08-06 22:40:19 +0800

    Commit all files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+ firebaseConfig : {
+  apiKey: "AIzaSyBnV4FEa2n2T71QeNyqSzhXJOvnmG-WMH0",
+  authDomain: "myapp-7165c.firebaseapp.com",
+  databaseURL: "https://myapp-7165c-default-rtdb.firebaseio.com",
+  projectId: "myapp-7165c",
+  storageBucket: "myapp-7165c.appspot.com",
+  messagingSenderId: "957465147197",
+  appId: "1:957465147197:web:b4b137697db1ba2f157754",
+  measurementId: "G-JHKMF0RTDY"
+}
 };
+  
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebaseConfig : {
+  apiKey: "AIzaSyBnV4FEa2n2T71QeNyqSzhXJOvnmG-WMH0",
+  authDomain: "myapp-7165c.firebaseapp.com",
+  databaseURL: "https://myapp-7165c-default-rtdb.firebaseio.com",
+  projectId: "myapp-7165c",
+  storageBucket: "myapp-7165c.appspot.com",
+  messagingSenderId: "957465147197",
+  appId: "1:957465147197:web:b4b137697db1ba2f157754",
+  measurementId: "G-JHKMF0RTDY"
+}
 };
+  
+
 
 /*
  * For easier debugging in development mode, you can import the following file
